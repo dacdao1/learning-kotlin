be47a9f3e2df36448ed678157875f97d490ac5ef
DacD <dacdao01@gmail.com>
2025-12-19 13:26:51 -0500

    adding from tut

---
+++ src/main/kotlin/Classes.kt
@@ -0,0 +1,13 @@
+class Car {
+    var name = ""
+    var model = ""
+    var color = ""
+    var doors = 0
+
+    fun move(){
+        println("car is moving")
+    }
+    fun stop(){
+        println("car is stopped")
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -73,22 +73,145 @@ fun main() {
 //    println(sum);
 
     // learning about class and oop
-    val car1 = Car()
-    car1.move()
+//    val car1 = Car()
+//    car1.name = "Tesla"
+//    car1.move()
+
+//    val user = User("dac", "dao", 21)
+//    val friend = User("Dac")
+//    val friend1 = User("Dac", "Dao")
+//    friend1.favoriteMovie = "hello"
+
+    // using function without initiate the class first
+//    User.sum(4,10)
+
+    // creating private class
+//    val instance = Database.getInstance()
+
+    // singleton of the object
+//    println(Database)
+
+    // lazy initialization
+//    val user by lazy {
+//        NewUser("dac")
+//    }
+//    println(user)
+
+    // enum, set constant and more powerful, have properties and function rather then val
+//    println(Direction.NORTH.ordinal)
+//    Direction.WEST.printData()
+
+    // when condition is like switch for enum but you have to include all of the enum
+//    val directionWhen = Direction.WEST
+    //OR
+//    val direction = Direction.valueOf("north".uppercase())
+//    when(directionWhen) {
+//        Direction.WEST -> print("direction is west")
+//        Direction.EAST -> print("direction is east")
+//        Direction.SOUTH -> print("direction is south")
+//        Direction.NORTH -> print("direction is north")
+//    }
+
+    // inner classes
+//    val listView = ListView(arrayOf("name 1", "name 2", "name 3"))
+//    listView.ListViewItems().displayItem(1)
+
+    // inherent
+    val car = CarInhe("toyo", "red", 1, 3)
+    car.move()
+
 }
 
-class Car {
-    var name = ""
-    var model = ""
-    var color = ""
-    var doors = 0
+// inheretent
+open class Vehicle(val name: String, val color: String){
+    //override this function in the child class
+    open fun move(){
+        println("$name is moving")
+    }
+    open fun stop(){
+        println("$name has stopped")
+    }
+}
 
-    fun move(){
-        println("car is moving")
+class CarInhe(name: String, color: String, val engines: Int, val doors: Int):Vehicle(name, color){
+    override fun move() {
+        driving()
+        super.move()
     }
-    fun stop(){
-        println("car is stopped")
+    fun driving(){
+        println("Driving the $name car")
     }
+}
+class PlaneInhe(name: String, color: String, val engines: Int, val doors: Int):Vehicle(name, color)
+
+class ListView(val items: Array<String>){
+    inner class ListViewItems(){
+        fun displayItem(position: Int){
+            println(items[position])
+        }
+    }
+}
+
+enum class Direction(var direction: String, var distance: Int) {
+    NORTH("north", 11),
+    SOUTH("south", 12),
+    EAST("east", 13),
+    WEST("west",14);
+
+    fun printData() {
+        println("Direction = $direction and $distance")
+    }
+
+}
+
+// lazy init class
+class NewUser(var firstName: String){
+    init{
+        println("User : $firstName was created")
+    }
+}
+
+//class Database private constructor(){
+//    companion object{
+//        private var instance: Database? = null
+//        fun getInstance(): Database?{
+//            if(instance == null){
+//                instance = Database()
+//            }
+//            return instance
+//        }
+//    }
+//}
+
+object Database {
+    init {
+        println("database created")
+    }
+}
+// primary constructor
+class User(var name: String, var lastName: String, var age:Int){
+
+    // create secondary constructor
+    constructor(name: String) : this(name, "LastName", 0)
+    constructor(name:String, lastName: String):this(name, lastName, 1)
+//    init{
+//        if(name.lowercase().startsWith("a")){
+//            this.name = name;
+//        } else{
+//            this.name = ""
+//            println("The name $name doesn't start with the letter 'a' or 'A'")
+//        }
+//    }
+
+    // having another variable within the class, late init. no primitive data types
+    lateinit var favoriteMovie: String
+    companion object {
+        fun sum(a: Int, b: Int): Int{
+            return a + b
+        }
+
+    }
+
 }
 
 fun sayHello(){
